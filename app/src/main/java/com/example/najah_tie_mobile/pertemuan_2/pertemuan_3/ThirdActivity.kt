146754f34825c564d9238ef566cff0a232d79a71
najah3tie <najah24ti@mahasiswa.pcr.ac.id>
2026-09-24 14:47:49 +0700

    First Commit

---
+++ app/src/main/java/com/example/najah_tie_mobile/pertemuan_2/pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,43 @@
+package com.example.najah_tie_mobile.pertemuan_2.pertemuan_3
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.najah_tie_mobile.R
+import com.example.najah_tie_mobile.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Inisialisasi komponen
+        val inputNoTujuan: EditText = findViewById(R.id.inputNoTujuan)
+        val btnKirim: Button = findViewById(R.id.btnKirim)
+        binding.btnKirim.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            val nomor = binding.inputNoTujuan.text
+            Log.e("Klik btnKirim","Tombol berhasil di tekan. Isi dari inputNama = $nomor")
+
+            Toast.makeText(this, "Pesan anda telah terkirim ke $nomor", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
